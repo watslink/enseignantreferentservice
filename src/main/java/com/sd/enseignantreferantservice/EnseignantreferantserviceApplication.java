9a85aa63aa58e1ad8d1b870855f667cca5c5a9e5
watslink <seb.decroix@gmail.com>
2019-09-24 14:05:13 +0200

    initial commit start project

---
+++ src/main/java/com/sd/enseignantreferantservice/EnseignantreferantserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sd.enseignantreferantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnseignantreferantserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnseignantreferantserviceApplication.class, args);
+    }
+
+}
