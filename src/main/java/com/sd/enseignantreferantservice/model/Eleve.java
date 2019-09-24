a2a3061ae84efa1d8163cfe1a492f38e6ec32a78
watslink <seb.decroix@gmail.com>
2019-09-24 16:16:03 +0200

    class model Eleve

---
+++ src/main/java/com/sd/enseignantreferantservice/model/Eleve.java
@@ -0,0 +1,71 @@
+package com.sd.enseignantreferantservice.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@Entity
+public class Eleve {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int eleveId;
+
+    private String nom;
+    private String prenom;
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+
+    private boolean dossierAccepte;
+    private boolean vu;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateReunion;
+
+    private String commentaire;
+
+    @ManyToOne
+    @JoinColumn(name="niveau_id")
+    private Niveau niveau;
+
+    @ManyToOne
+    @JoinColumn(name="etablissement_id")
+    private Etablissement etablissement;
+
+    @ManyToOne
+    @JoinColumn(name="AESH_id")
+    private AESH aesh;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateNotificationAesh;
+
+    @ManyToOne
+    @JoinColumn(name="enseignant_referent_id")
+    private EnseignantReferent enseignantReferent;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "eleve_representant_legal",
+            joinColumns = {@JoinColumn(name = "eleve_id")},
+            inverseJoinColumns = {@JoinColumn(name = "representant_legal_id")})
+    private Set<RepresentantLegal> listRepresentantsLegaux;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "eleve_structure_pro",
+            joinColumns = {@JoinColumn(name = "eleve_id")},
+            inverseJoinColumns = {@JoinColumn(name = "structure_pro_id")})
+    private Set<StructurePro> listStructurePros;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "eleve_materiel_pedago_adapte",
+            joinColumns = {@JoinColumn(name = "eleve_id")},
+            inverseJoinColumns = {@JoinColumn(name = "materiel_pedago_adapte_id")})
+    private Set<MaterielPedagoAdapte> listMaterielsPedagoAdaptes;
+
+    @OneToMany(mappedBy = "document", orphanRemoval = true)
+    private Set<Document> listDocuments;
+
+    @OneToMany(mappedBy = "eleve_document_inscription_requis", orphanRemoval = true)
+    private Set<EleveDocumentInscriptionRequis> listEleveDocumentsInscriptionRequis;
+}
