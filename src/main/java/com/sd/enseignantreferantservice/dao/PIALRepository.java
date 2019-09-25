179300be23b4b2b3226375b1ec0fe499056cbeb3
watslink <seb.decroix@gmail.com>
2019-09-25 11:21:21 +0200

    create PIALRepository

--- src/main/java/com/sd/enseignantreferantservice/dao/PIALRepository.java
+++ src/main/java/com/sd/enseignantreferantservice/dao/PIALRepository.java
@@ -1,9 +1,10 @@
 package com.sd.enseignantreferantservice.dao;
 
-import com.sd.enseignantreferantservice.model.Niveau;
+import com.sd.enseignantreferantservice.model.PIAL;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NiveauRepository extends JpaRepository<Niveau, Integer> {
+public interface PIALRepository  extends JpaRepository<PIAL, Integer> {
 }
