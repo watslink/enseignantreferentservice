d28afab450d32171f83130d97a850f2e048d3957
watslink <seb.decroix@gmail.com>
2019-10-01 14:38:22 +0200

    add DocumentService + implementation

---
+++ src/main/java/com/sd/enseignantreferantservice/business/Interface/DocumentService.java
@@ -0,0 +1,16 @@
+package com.sd.enseignantreferantservice.business.Interface;
+
+import com.sd.enseignantreferantservice.model.Document;
+
+import java.util.List;
+
+public interface DocumentService {
+
+    Document addDocument(Document document);
+    void deleteDocument (Document document);
+    Document updateDocument (Document document);
+
+    Document getDocument(int id);
+
+    List<Document> getAllDocument();
+}
---
+++ src/main/java/com/sd/enseignantreferantservice/business/implementation/DocumentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.sd.enseignantreferantservice.business.implementation;
+
+import com.sd.enseignantreferantservice.business.Interface.DocumentService;
+import com.sd.enseignantreferantservice.dao.DocumentRepository;
+import com.sd.enseignantreferantservice.model.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class DocumentServiceImpl implements DocumentService {
+
+    @Autowired
+    DocumentRepository documentRepository;
+
+    @Override
+    public Document addDocument(Document document) {
+        return documentRepository.save(document);
+    }
+
+    @Override
+    public void deleteDocument(Document document) {
+        documentRepository.delete(document);
+    }
+
+    @Override
+    public Document updateDocument(Document document) {
+        return documentRepository.save(document);
+    }
+
+    @Override
+    public Document getDocument(int id) {
+        Optional<Document> optionalDocument=documentRepository.findById(id);
+        return optionalDocument.orElse(null) ;
+    }
+
+    @Override
+    public List<Document> getAllDocument() {
+        return documentRepository.findAll();
+    }
+}
