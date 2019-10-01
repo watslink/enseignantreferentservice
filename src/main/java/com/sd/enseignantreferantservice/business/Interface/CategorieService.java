6a64ddca96edd5e51abb07ab909fdaa73b91aff1
watslink <seb.decroix@gmail.com>
2019-10-01 13:17:18 +0200

    add CategorieService + implementation

---
+++ src/main/java/com/sd/enseignantreferantservice/business/Interface/CategorieService.java
@@ -0,0 +1,16 @@
+package com.sd.enseignantreferantservice.business.Interface;
+
+import com.sd.enseignantreferantservice.model.Categorie;
+
+import java.util.List;
+
+public interface CategorieService {
+
+    Categorie addCategorie(Categorie categorie);
+    void deleteCategorie (Categorie categorie);
+    Categorie updateCategorie (Categorie categorie);
+
+    Categorie getCategorie(int id);
+
+    List<Categorie> getAllCategorie();
+}
---
+++ src/main/java/com/sd/enseignantreferantservice/business/implementation/CategorieServiceImpl.java
@@ -0,0 +1,41 @@
+package com.sd.enseignantreferantservice.business.implementation;
+
+import com.sd.enseignantreferantservice.business.Interface.CategorieService;
+import com.sd.enseignantreferantservice.dao.CategorieRepository;
+import com.sd.enseignantreferantservice.model.Categorie;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CategorieServiceImpl implements CategorieService {
+
+    @Autowired
+    CategorieRepository categorieRepository;
+
+    @Override
+    public Categorie addCategorie(Categorie categorie) {
+        return categorieRepository.save(categorie);
+    }
+
+    @Override
+    public void deleteCategorie(Categorie categorie) {
+        categorieRepository.delete(categorie);
+    }
+
+    @Override
+    public Categorie updateCategorie(Categorie categorie) {
+        return categorieRepository.save(categorie);
+    }
+
+    @Override
+    public Categorie getCategorie(int id) {
+        Optional<Categorie> optionalCategorie=categorieRepository.findById(id);
+        return optionalCategorie.orElse(null) ;
+    }
+
+    @Override
+    public List<Categorie> getAllCategorie() {
+        return categorieRepository.findAll();
+    }
+}
