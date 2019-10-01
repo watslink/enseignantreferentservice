f33a362e99e4dfd7f12fae05ab21e383ed08eb8c
watslink <seb.decroix@gmail.com>
2019-10-01 12:52:24 +0200

    add method findByDossierAccepte

--- src/main/java/com/sd/enseignantreferantservice/dao/EleveRepository.java
+++ src/main/java/com/sd/enseignantreferantservice/dao/EleveRepository.java
@@ -4,6 +4,10 @@ import com.sd.enseignantreferantservice.model.Eleve;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EleveRepository extends JpaRepository<Eleve, Integer> {
+
+    List<Eleve> findByDossierAccepte(boolean bool);
 }
