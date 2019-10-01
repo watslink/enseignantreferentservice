e7e2732032ccf1a9f0c154da096f17ed583a79df
watslink <seb.decroix@gmail.com>
2019-10-01 15:42:19 +0200

    add securityConstants + jwt filters

---
+++ src/main/java/com/sd/enseignantreferantservice/security/JWTAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.sd.enseignantreferantservice.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sd.enseignantreferantservice.model.EnseignantReferent;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        EnseignantReferent ensRef=null;
+        try {
+            ensRef=new ObjectMapper().readValue(request.getInputStream(), EnseignantReferent.class);
+        } catch (Exception e) {
+           throw new RuntimeException(e);
+        }
+
+        return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(ensRef.getIdentifiant(), ensRef.getMotDePasse()));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        MyUserDetails springUser=(MyUserDetails)authResult.getPrincipal();
+        String jwt= Jwts.builder()
+                .setSubject(springUser.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis()+SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SecurityConstants.SECRET)
+                .claim("roles", springUser.getAuthorities())
+                .compact();
+        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX+jwt);
+
+    }
+}
---
+++ src/main/java/com/sd/enseignantreferantservice/security/JWTAuthorizationFilter.java
@@ -0,0 +1,57 @@
+package com.sd.enseignantreferantservice.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type," +
+                "Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        httpServletResponse.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
+
+        String jwt=httpServletRequest.getHeader(SecurityConstants.HEADER_STRING);
+
+        if(httpServletRequest.getMethod().equals("OPTIONS")){
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        }
+        else{
+            if(jwt==null || !jwt.startsWith(SecurityConstants.TOKEN_PREFIX)){
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+            }
+            else{
+                Claims claims= Jwts.parser().setSigningKey(SecurityConstants.SECRET)
+                        .parseClaimsJws(jwt.replace(SecurityConstants.TOKEN_PREFIX, ""))
+                        .getBody();
+                String userIdentifiant=claims.getSubject();
+
+                Set<GrantedAuthority> authorities = new HashSet<>();
+                authorities.add(new SimpleGrantedAuthority("User"));
+
+                UsernamePasswordAuthenticationToken authenticationToken=
+                        new UsernamePasswordAuthenticationToken(userIdentifiant, null, authorities);
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+            }
+        }
+
+    }
+}
