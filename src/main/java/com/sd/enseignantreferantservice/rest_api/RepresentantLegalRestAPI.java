9f61d32dd2ff7bc0a43de056b4d1f63c9668bf33
watslink <seb.decroix@gmail.com>
2019-10-02 14:32:45 +0200

    create RepresentantLegalRestAPI

---
+++ src/main/java/com/sd/enseignantreferantservice/rest_api/RepresentantLegalRestAPI.java
@@ -0,0 +1,44 @@
+package com.sd.enseignantreferantservice.rest_api;
+
+import com.sd.enseignantreferantservice.business.serviceInterface.RepresentantLegalService;
+import com.sd.enseignantreferantservice.model.RepresentantLegal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class RepresentantLegalRestAPI {
+
+
+    @Autowired
+    private RepresentantLegalService representantLegalService;
+
+    @GetMapping("/representantLegal/{id}")
+    public RepresentantLegal getRepresentantLegal(@PathVariable int id){
+        return  representantLegalService.getRepresentantLegal(id);
+    }
+
+    @GetMapping("/representantLegaux")
+    public List<RepresentantLegal> getListRepresentantLegal() {
+        return representantLegalService.getAllRepresentantLegal();
+    }
+
+
+    @PostMapping("/representantLegal")
+    public RepresentantLegal addRepresentantLegal(@RequestBody RepresentantLegal representantLegal){
+        return representantLegalService.addRepresentantLegal(representantLegal);
+    }
+
+    @PutMapping("/representantLegal")
+    public RepresentantLegal updateRepresentantLegal(@RequestBody RepresentantLegal representantLegal){
+        return representantLegalService.updateRepresentantLegal(representantLegal);
+    }
+
+    @DeleteMapping("/representantLegal")
+    public void deleteRepresentantLegal(@RequestBody RepresentantLegal representantLegal){
+        representantLegalService.deleteRepresentantLegal(representantLegal);
+    }
+}
+
+
