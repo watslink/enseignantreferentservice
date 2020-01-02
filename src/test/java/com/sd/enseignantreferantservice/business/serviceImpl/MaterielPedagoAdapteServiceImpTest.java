d366ceb1141b832642d8f27757ea145e26aa7d26
watslink <seb.decroix@gmail.com>
2020-01-02 11:35:22 +0100

    implement MaterielPedagoAdapteServiceImplTest

--- src/test/java/com/sd/enseignantreferantservice/business/serviceImpl/MaterielPedagoAdapteServiceImpTest.java
+++ src/test/java/com/sd/enseignantreferantservice/business/serviceImpl/MaterielPedagoAdapteServiceImpTest.java
@@ -1,9 +1,14 @@
 package com.sd.enseignantreferantservice.business.serviceImpl;
 
+import com.sd.enseignantreferantservice.model.EnseignantReferent;
+import com.sd.enseignantreferantservice.model.MaterielPedagoAdapte;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -11,23 +16,53 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class MaterielPedagoAdapteServiceImpTest {
 
+    @Autowired
+    MaterielPedagoAdapteServiceImp materielPedagoAdapteService;
+
     @Test
+    @Rollback
+    @Transactional
     public void addMaterielPedagoAdapte() {
+        MaterielPedagoAdapte materielPedagoAdapte = new MaterielPedagoAdapte();
+        materielPedagoAdapte.setNom("nom");
+        EnseignantReferent ensRef= new EnseignantReferent();
+        ensRef.setEnseignantReferentId(1);
+        materielPedagoAdapte.setEnseignantReferent(ensRef);
+        MaterielPedagoAdapte materielPedagoAdapteSaved = materielPedagoAdapteService.addMaterielPedagoAdapte(materielPedagoAdapte);
+        assertEquals("nom", materielPedagoAdapteSaved.getNom());
+        assertEquals(ensRef, materielPedagoAdapteSaved.getEnseignantReferent());
     }
 
     @Test
+    @Rollback
+    @Transactional
     public void deleteMaterielPedagoAdapte() {
+        materielPedagoAdapteService.deleteMaterielPedagoAdapte(1);
+        assertNull(materielPedagoAdapteService.getMaterielPedagoAdapte(1));
     }
 
     @Test
+    @Rollback
+    @Transactional
     public void updateMaterielPedagoAdapte() {
+        MaterielPedagoAdapte materielPedagoAdapte = materielPedagoAdapteService.getMaterielPedagoAdapte(1);
+        materielPedagoAdapte.setNom("updated");
+        MaterielPedagoAdapte materielPedagoAdapteUpdated = materielPedagoAdapteService.updateMaterielPedagoAdapte(materielPedagoAdapte);
+        assertEquals("updated", materielPedagoAdapteUpdated.getNom());
     }
 
     @Test
     public void getMaterielPedagoAdapte() {
+        assertEquals("ordinateur", materielPedagoAdapteService.getMaterielPedagoAdapte(1).getNom());
+    }
+
+    @Test
+    public void getMaterielPedagoAdapteWithIdNotExist() {
+        assertNull(materielPedagoAdapteService.getMaterielPedagoAdapte(75));
     }
 
     @Test
     public void getAllMaterielPedagoAdapte() {
+        assertEquals(4, materielPedagoAdapteService.getAllMaterielPedagoAdapte(1).size());
     }
 }
