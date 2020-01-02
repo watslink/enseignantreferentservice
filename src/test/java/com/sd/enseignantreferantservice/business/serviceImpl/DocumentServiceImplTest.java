30200ee13000665e94552ca5ad23f4be68743938
watslink <seb.decroix@gmail.com>
2020-01-02 10:58:53 +0100

    add spring test context

--- src/test/java/com/sd/enseignantreferantservice/business/serviceImpl/DocumentServiceImplTest.java
+++ src/test/java/com/sd/enseignantreferantservice/business/serviceImpl/DocumentServiceImplTest.java
@@ -1,9 +1,14 @@
 package com.sd.enseignantreferantservice.business.serviceImpl;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DocumentServiceImplTest {
 
     @Test
