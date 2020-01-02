017c9d465f24cc1b0bfd6f1867daa3c64db63e65
watslink <seb.decroix@gmail.com>
2020-01-02 09:32:27 +0100

    update travis

--- src/main/java/com/sd/enseignantreferantservice/EnseignantreferantserviceApplication.java
+++ src/main/java/com/sd/enseignantreferantservice/EnseignantreferantserviceApplication.java
@@ -1,21 +1,9 @@
 package com.sd.enseignantreferantservice;
 
-import com.sd.enseignantreferantservice.dao.EleveRepository;
-import com.sd.enseignantreferantservice.dao.EnseignantReferentRepository;
-import com.sd.enseignantreferantservice.dao.MaterielPedagoAdapteRepository;
-import com.sd.enseignantreferantservice.model.Eleve;
-import com.sd.enseignantreferantservice.model.MaterielPedagoAdapte;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import static com.sun.xml.internal.ws.policy.sourcemodel.wspolicy.XmlToken.Optional;
 
 @SpringBootApplication
 public class EnseignantreferantserviceApplication implements CommandLineRunner {
@@ -24,27 +12,10 @@ public class EnseignantreferantserviceApplication implements CommandLineRunner {
         SpringApplication.run(EnseignantreferantserviceApplication.class, args);
     }
 
-    @Autowired
-    EnseignantReferentRepository enseignantReferentRepository;
 
-    @Autowired
-    EleveRepository eleveRepository;
-
-    @Autowired
-    MaterielPedagoAdapteRepository materielPedagoAdapteRepository;
 
     @Override
-    @Transactional
     public void run(String... args) throws Exception {
-       java.util.Optional<Eleve> optEleve=eleveRepository.findById(1);
-       Eleve eleve=  optEleve.get();
-       System.out.println(eleve.toString());
-        System.out.println(eleve.toString());
-        System.out.println(eleve.getNom());
-        System.out.println(eleve.getPrenom());
-        System.out.println(eleve.getDateNaissance());
-        System.out.println(eleve.getNiveau());
-        System.out.println(eleve.getDateReunion());
 
     }
 }
