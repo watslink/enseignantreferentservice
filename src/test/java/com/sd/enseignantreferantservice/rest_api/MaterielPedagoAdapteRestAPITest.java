2b904409072d17d6f90dcf4c1d7f96bf19da394c
watslink <seb.decroix@gmail.com>
2020-01-04 18:33:41 +0100

    implement MaterielPedagoAdapteReferentRestAPITest

--- src/test/java/com/sd/enseignantreferantservice/rest_api/MaterielPedagoAdapteRestAPITest.java
+++ src/test/java/com/sd/enseignantreferantservice/rest_api/MaterielPedagoAdapteRestAPITest.java
@@ -1,15 +1,25 @@
 package com.sd.enseignantreferantservice.rest_api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sd.enseignantreferantservice.model.EnseignantReferent;
+import com.sd.enseignantreferantservice.model.MaterielPedagoAdapte;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WithMockUser
@@ -20,22 +30,80 @@ public class MaterielPedagoAdapteRestAPITest {
     private MockMvc mvc;
 
     @Test
-    public void getMaterielPedagoAdapte() {
+    public void getMaterielPedagoAdapte() throws Exception  {
+        mvc.perform(get("/materielPedagoAdapte/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("ordinateur"));
     }
 
     @Test
-    public void getListMaterielPedagoAdapte() {
+    public void getListMaterielPedagoAdapte() throws Exception  {
+        mvc.perform(get("/materielPedagoAdaptes/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)));
     }
 
     @Test
-    public void addMaterielPedagoAdapte() {
+    @Rollback
+    @Transactional
+    public void addMaterielPedagoAdapte() throws Exception  {
+        MaterielPedagoAdapte materielPedagoAdapte = new MaterielPedagoAdapte();
+        materielPedagoAdapte.setNom("nom");
+        EnseignantReferent ensRef= new EnseignantReferent();
+        ensRef.setEnseignantReferentId(1);
+        materielPedagoAdapte.setEnseignantReferent(ensRef);
+        mvc.perform(post("/materielPedagoAdapte" )
+                .content(asJsonString(materielPedagoAdapte))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("nom"));
     }
 
     @Test
-    public void updateMaterielPedagoAdapte() {
+    @Rollback
+    @Transactional
+    public void updateMaterielPedagoAdapte() throws Exception  {
+        MaterielPedagoAdapte materielPedagoAdapte = new MaterielPedagoAdapte();
+        materielPedagoAdapte.setNom("nom");
+        EnseignantReferent ensRef= new EnseignantReferent();
+        ensRef.setEnseignantReferentId(1);
+        materielPedagoAdapte.setEnseignantReferent(ensRef);
+        materielPedagoAdapte.setMaterielPedagoAdapteId(1);
+        mvc.perform(put("/materielPedagoAdapte" )
+                .content(asJsonString(materielPedagoAdapte))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("nom"));
+        mvc.perform(get("/materielPedagoAdapte/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("nom"));
     }
 
     @Test
-    public void deleteMaterielPedagoAdapte() {
+    @Rollback
+    @Transactional
+    public void deleteMaterielPedagoAdapte() throws Exception  {
+        mvc.perform(delete("/materielPedagoAdapte/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mvc.perform(get("/materielPedagoAdapte/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
