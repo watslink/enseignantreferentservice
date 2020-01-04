324d72a3bdb5f6d8fe9e0d337aafd6c408ad81ad
watslink <seb.decroix@gmail.com>
2020-01-04 18:31:37 +0100

    implement EtablissementReferentRestAPITest

--- src/test/java/com/sd/enseignantreferantservice/rest_api/EtablissementRestAPITest.java
+++ src/test/java/com/sd/enseignantreferantservice/rest_api/EtablissementRestAPITest.java
@@ -1,15 +1,28 @@
 package com.sd.enseignantreferantservice.rest_api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sd.enseignantreferantservice.model.Adresse;
+import com.sd.enseignantreferantservice.model.EnseignantReferent;
+import com.sd.enseignantreferantservice.model.Etablissement;
+import com.sd.enseignantreferantservice.model.PIAL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WithMockUser
@@ -20,22 +33,104 @@ public class EtablissementRestAPITest {
     private MockMvc mvc;
 
     @Test
-    public void getEtablissement() {
+    public void getEtablissement() throws Exception  {
+        mvc.perform(get("/etablissement/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("Boyer Malo"));
     }
 
     @Test
-    public void getListEtablissement() {
+    public void getListEtablissement() throws Exception  {
+        mvc.perform(get("/etablissements/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(15)));
     }
 
     @Test
-    public void addEtablissement() {
+    @Rollback
+    @Transactional
+    public void addEtablissement() throws Exception  {
+        Etablissement etablissement = new Etablissement();
+        PIAL pial=new PIAL();
+        pial.setPialId(1);
+        etablissement.setPial(pial);
+        EnseignantReferent ensRef= new EnseignantReferent();
+        ensRef.setEnseignantReferentId(1);
+        etablissement.setEnseignantReferent(ensRef);
+        etablissement.setMail("mail@mail.com");
+        etablissement.setTelephone("06.06.06.06.06");
+        Adresse adresse = new Adresse();
+        adresse.setCodePostal(59000);
+        adresse.setNumero(9);
+        adresse.setVille("Lille");
+        adresse.setVoie("rue rue");
+        etablissement.setAdresse(adresse);
+        etablissement.setNom("nom");
+        etablissement.setRne("1234567R");
+        mvc.perform(post("/etablissement" )
+                .content(asJsonString(etablissement))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("nom"));
     }
 
     @Test
-    public void updateEtablissement() {
+    @Rollback
+    @Transactional
+    public void updateEtablissement() throws Exception  {
+        Etablissement etablissement = new Etablissement();
+        PIAL pial=new PIAL();
+        pial.setPialId(1);
+        etablissement.setPial(pial);
+        EnseignantReferent ensRef= new EnseignantReferent();
+        ensRef.setEnseignantReferentId(1);
+        etablissement.setEnseignantReferent(ensRef);
+        etablissement.setMail("mail@mail.com");
+        etablissement.setTelephone("06.06.06.06.06");
+        Adresse adresse = new Adresse();
+        adresse.setCodePostal(59000);
+        adresse.setNumero(9);
+        adresse.setVille("Lille");
+        adresse.setVoie("rue rue");
+        etablissement.setAdresse(adresse);
+        etablissement.setNom("nom");
+        etablissement.setRne("1234567R");
+        etablissement.setEtablissementId(1);
+        mvc.perform(put("/etablissement" )
+                .content(asJsonString(etablissement))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("nom"));
+        mvc.perform(get("/etablissement/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nom").value("nom"));
     }
 
     @Test
-    public void deleteEtablissement() {
+    @Rollback
+    @Transactional
+    public void deleteEtablissement() throws Exception  {
+        mvc.perform(delete("/etablissement/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mvc.perform(get("/etablissement/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
