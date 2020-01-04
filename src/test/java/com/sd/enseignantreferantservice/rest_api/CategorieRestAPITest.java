3ec2edf45c5e6e71765e1dcf4db70d6a58e294de
watslink <seb.decroix@gmail.com>
2020-01-04 15:11:33 +0100

    add mockMvc injection

--- src/test/java/com/sd/enseignantreferantservice/rest_api/CategorieRestAPITest.java
+++ src/test/java/com/sd/enseignantreferantservice/rest_api/CategorieRestAPITest.java
@@ -1,10 +1,15 @@
 package com.sd.enseignantreferantservice.rest_api;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 
 public class CategorieRestAPITest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void getCategorie() {
--- src/test/java/com/sd/enseignantreferantservice/rest_api/EtablissementRestAPITest.java
+++ src/test/java/com/sd/enseignantreferantservice/rest_api/EtablissementRestAPITest.java
@@ -2,10 +2,12 @@ package com.sd.enseignantreferantservice.rest_api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
@@ -13,6 +15,9 @@ import static org.junit.Assert.*;
 @WithMockUser
 @AutoConfigureMockMvc
 public class EtablissementRestAPITest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void getEtablissement() {
